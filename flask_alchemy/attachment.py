54cb52463ad95edbabeedf574451d343f9265165
Konsta Vesterinen <konsta.vesterinen@gmail.com>
2012-11-13 14:12:51 +0200

    commented out attachements for now

--- flask_alchemy/attachment.py
+++ flask_alchemy/attachment.py
@@ -1,76 +1,76 @@
-from datetime import datetime
-import os
+# from datetime import datetime
+# import os
 
-from flask import url_for
-from flask.ext.storage import StorageException
-from flask.ext.login import current_user
-from wtforms_alchemy import ModelForm
-from wtforms.fields import FileField
-from wtforms.validators import DataRequired, Optional
+# from flask import url_for
+# from flask.ext.storage import StorageException
+# from flask.ext.login import current_user
+# from wtforms_alchemy import ModelForm
+# from wtforms.fields import FileField
+# from wtforms.validators import DataRequired, Optional
 
-from ..extensions import db, storage
-
-
-class AttachmentMixin(object):
-    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-
-    type = db.Column(db.Unicode(255), nullable=False, index=True)
-    key = db.Column(db.UnicodeText, nullable=False)
-    size = db.Column(db.Integer, nullable=False)
-
-    uploader_id = db.Column(None, db.ForeignKey('user.id'), nullable=False)
-
-    uploader = db.relationship('User')
-
-    created_at = db.Column(
-        db.DateTime, nullable=False, default=datetime.utcnow
-    )
-
-    @property
-    def delete_modal_name(self):
-        return 'attachment-%d' % self.id
-
-    @property
-    def delete_url(self):
-        return url_for(
-            'attachment.delete_file', id=self.id,
-        )
-
-    @property
-    def name(self):
-        _, tail = os.path.split(self.key)
-        return tail
-
-    @property
-    def url(self):
-        return storage.url(self.key)
+# from ..extensions import db, storage
 
 
-class AttachmentForm(ModelForm):
-    class Meta:
-        model = Attachment
-        exclude = ['key', 'type', 'size']
+# class AttachmentMixin(object):
+#     id = db.Column(db.Integer, autoincrement=True, primary_key=True)
 
-    file = FileField(u'Picture', validators=[DataRequired()])
+#     type = db.Column(db.Unicode(255), nullable=False, index=True)
+#     key = db.Column(db.UnicodeText, nullable=False)
+#     size = db.Column(db.Integer, nullable=False)
 
-    def populate_obj(self, obj):
-        file_ = self.file.data
+#     uploader_id = db.Column(None, db.ForeignKey('user.id'), nullable=False)
 
-        if file_:
-            contents = file_.read()
-            if obj.id:
-                try:
-                    storage.delete(obj.name)
-                except StorageException:
-                    pass
-            new_file = storage.save(file_.filename, contents)
-            obj.key = unicode(new_file.name)
-            obj.size = len(contents)
-            obj.uploader = current_user
+#     uploader = db.relationship('User')
+
+#     created_at = db.Column(
+#         db.DateTime, nullable=False, default=datetime.utcnow
+#     )
+
+#     @property
+#     def delete_modal_name(self):
+#         return 'attachment-%d' % self.id
+
+#     @property
+#     def delete_url(self):
+#         return url_for(
+#             'attachment.delete_file', id=self.id,
+#         )
+
+#     @property
+#     def name(self):
+#         _, tail = os.path.split(self.key)
+#         return tail
+
+#     @property
+#     def url(self):
+#         return storage.url(self.key)
 
 
-def attachment_form_factory(name, validators=[Optional()]):
-    class OptionalAttachmentForm(AttachmentForm):
-        file = FileField(name, validators=validators)
+# class AttachmentForm(ModelForm):
+#     class Meta:
+#         model = Attachment
+#         exclude = ['key', 'type', 'size']
 
-    return OptionalAttachmentForm
+#     file = FileField(u'Picture', validators=[DataRequired()])
+
+#     def populate_obj(self, obj):
+#         file_ = self.file.data
+
+#         if file_:
+#             contents = file_.read()
+#             if obj.id:
+#                 try:
+#                     storage.delete(obj.name)
+#                 except StorageException:
+#                     pass
+#             new_file = storage.save(file_.filename, contents)
+#             obj.key = unicode(new_file.name)
+#             obj.size = len(contents)
+#             obj.uploader = current_user
+
+
+# def attachment_form_factory(name, validators=[Optional()]):
+#     class OptionalAttachmentForm(AttachmentForm):
+#         file = FileField(name, validators=validators)
+
+#     return OptionalAttachmentForm
